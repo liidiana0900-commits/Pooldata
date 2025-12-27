420c3b6e915f3d1b75f02d057837da6b79d0b418
liidiana0900-commits <liidiana0900@gmail.com>
2025-12-28 02:40:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template_string
 from datetime import date
+import os
 
 app = Flask(__name__)
 
@@ -85,6 +86,7 @@ def home():
 
 import os
 
-if name == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
